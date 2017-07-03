900bd35366644e7b51ebc80829b15465d17264d6
Jonathan Spruce <jonathan.spruce@sage.com>
2017-07-03 13:38:22 +0100

    First feature-complete version

--- lib/tennis_game.rb
+++ lib/tennis_game.rb
@@ -9,10 +9,51 @@ class TennisGame
     @points = [0, 0]
   end
 
+  # converts the score into a the tennis scores
   def score
-    "#{TENNIS_POINTS[@points[0]]} : #{TENNIS_POINTS[@points[1]]}"
+    if deuce?
+      'Deuce'
+    # if both teams have at least 3 points but are not equal
+    elsif advantage?
+      "Advantage Team #{which_team_advantage}"
+    elsif win?
+      "Team #{which_team_win} Win!"
+    else
+      # just show converted scores
+      "#{TENNIS_POINTS[@points[0]]} : #{TENNIS_POINTS[@points[1]]}"
+    end
   end
 
+  # checks whether the teams are currently at deuce:
+  # the teams both have at least 3 points and the points are equal
+  def deuce?
+    @points[0] >= 3 && @points[1] == @points[0]
+  end
+
+  # returns true or false depending on whether any team has the advantage
+  def advantage?
+    true unless which_team_advantage.nil?
+  end
+
+  # checks whether a team has an advantage, and passes the index of the
+  # team with the advantage
+  def which_team_advantage
+    # return the index of the team with the advantage,
+    # if there is only one point difference
+    @points.index(@points.max) if (@points[0] >= 3 && @points[1] >= 3) &&
+        (@points[0] - @points[1]).abs == 1
+  end
+
+  def win?
+    true unless which_team_win.nil?
+  end
+
+  def which_team_win
+    @points.index(@points.max) if (@points[0] >= 4 || @points[1] >= 4) &&
+        (@points[0] - @points[1]).abs >= 2
+  end
+
+  # used to add one point to the specified team
   def add_point(team)
     @points[team] += 1
   end
--- spec/tennis_game_spec.rb
+++ spec/tennis_game_spec.rb
@@ -13,15 +13,51 @@ describe TennisGame do
   end
   describe '#score' do
     context 'it converts the score into tennis scores' do
+      subject(:score) { tennis_game.score }
       context 'at the start of game' do
-        it 'should have a score of "Love" for both players' do
-          expect(tennis_game.score).to eq('Love : Love')
+        it { is_expected.to eq('Love : Love') }
+      end
+      context 'when score is "1 : 0"' do
+        before { tennis_game.points = [1, 0] }
+        it { is_expected.to eq('Fifteen : Love') }
+      end
+      context 'when score is "2 : 0"' do
+        before { tennis_game.points = [2, 0] }
+        it { is_expected.to eq('Thirty : Love') }
+      end
+      context 'when score is "3 : 0"' do
+        before { tennis_game.points = [3, 0] }
+        it { is_expected.to eq('Fourty : Love') }
+      end
+      context 'when deuce' do
+        context 'when score is equal at 3 points' do
+          before { tennis_game.points = [3, 3] }
+          it { is_expected.to eq('Deuce') }
+        end
+        context 'when score is equal at 10 points' do
+          before { tennis_game.points = [10, 10] }
+          it { is_expected.to eq('Deuce') }
         end
       end
-      context 'when a team has one point' do
-        it 'should output "Fifteen" for that team' do
-          tennis_game.add_point(0)
-          expect(tennis_game.score).to eq('Fifteen : Love')
+      context 'when advantage' do
+        context 'when the score is "4 : 3"' do
+          before { tennis_game.points = [4, 3] }
+          it { is_expected.to eq('Advantage Team 0') }
+        end
+        context 'when the score is "4 : 5"' do
+          before { tennis_game.points = [4, 5] }
+          it { is_expected.to eq('Advantage Team 1') }
+        end
+      end
+      context 'when a team has won' do
+        context 'when the score is "4 : 0"' do
+          before { tennis_game.points = [4, 0] }
+          it { is_expected.to eq('Team 0 Win!') }
+        end
+
+        context 'when the score is "4 : 6"' do
+          before { tennis_game.points = [4, 6] }
+          it { is_expected.to eq('Team 1 Win!') }
         end
       end
     end
