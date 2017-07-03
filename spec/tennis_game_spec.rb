9ee415fa7e136ec2ce6d71970212ebfeea260835
Jonathan Spruce <jonathan.spruce@sage.com>
2017-07-03 16:59:03 +0100

    Fix spacing in tennis_game_spec.rb

--- spec/tennis_game_spec.rb
+++ spec/tennis_game_spec.rb
@@ -11,44 +11,54 @@ describe TennisGame do
       end
     end
   end
+
   describe '#score' do
     context 'it converts the score into tennis scores' do
       subject(:score) { tennis_game.score }
+
       context 'at the start of game' do
         it { is_expected.to eq('Love : Love') }
       end
+
       context 'when score is "1 : 0"' do
         before { tennis_game.points = [1, 0] }
         it { is_expected.to eq('Fifteen : Love') }
       end
+
       context 'when score is "2 : 0"' do
         before { tennis_game.points = [2, 0] }
         it { is_expected.to eq('Thirty : Love') }
       end
+
       context 'when score is "3 : 0"' do
         before { tennis_game.points = [3, 0] }
         it { is_expected.to eq('Fourty : Love') }
       end
+
       context 'when deuce' do
         context 'when score is equal at 3 points' do
           before { tennis_game.points = [3, 3] }
           it { is_expected.to eq('Deuce') }
         end
+
         context 'when score is equal at 10 points' do
           before { tennis_game.points = [10, 10] }
           it { is_expected.to eq('Deuce') }
         end
       end
+
       context 'when advantage' do
         context 'when the score is "4 : 3"' do
           before { tennis_game.points = [4, 3] }
           it { is_expected.to eq('Advantage Team 0') }
         end
+
         context 'when the score is "4 : 5"' do
           before { tennis_game.points = [4, 5] }
           it { is_expected.to eq('Advantage Team 1') }
         end
       end
+
       context 'when a team has won' do
         context 'when the score is "4 : 0"' do
           before { tennis_game.points = [4, 0] }
@@ -62,6 +72,7 @@ describe TennisGame do
       end
     end
   end
+
   describe '#add_point' do
     it 'should add one point to the specified team' do
       expect { tennis_game.add_point(0) }.to change { tennis_game.points[0] }
